6a224f55c16f3a218521216a8ac5e9e8debc89ba
Daniil Makovetskii <d.makovetskii@g.nsu.ru>
2025-11-30 17:35:54 +0700

    upd Auth

--- frontend/src/components/Auth/RegisterPage.js
+++ frontend/src/components/Auth/RegisterPage.js
@@ -12,7 +12,7 @@ export default function RegisterPage() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const res = await fetch("http://localhost:8000/auth/register", {
+    const res = await fetch(`${API_URL}/auth/register`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password }),
